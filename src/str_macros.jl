01ccbaf33d73fea91ec8a10ecdccdd990d359a52
Jarrett Revels <jarrettrevels@gmail.com>
2015-04-23 12:16:37 -0400

    refactored to use opdefexpr and opdefstr types

--- src/str_macros.jl
+++ src/str_macros.jl
@@ -30,18 +30,45 @@ macro d_mstr(str)
     end)
 end
 
-###########
-# @rep_op #
-###########
+###################################
+# Definition String Parsing Utils #
+###################################
+immutable OpDefStr
+    op_name::AbstractString
+    label_args::AbstractString
+    lhs_type::AbstractString
+    rhs::AbstractString
+end
 
-macro rep_op(str, bases...)
-    op_name, label_args, def_str, type_str = parse_defstr(str)
-    result_expr = rep_op_expr(op_name, label_args, def_str, type_str, build_prod_basis(bases...))
-    return esc(result_expr)
-end 
+function OpDefStr(str::AbstractString)
+    str = rm_whspace(str)
+    left, right = @compat(split(str, '=', limit=2))
 
-build_prod_basis(basis) = basis
-build_prod_basis(first, bases...) = :(Iterators.product($first, $(bases...)))
+    if ismatch(ktpat, left)
+        lhs_type = "Ket"
+    elseif ismatch(brpat, left)
+        lhs_type = "Bra"
+    else
+        error("Couldn't detect whether operator is acting on Bra or Ket...")
+    end
+
+    if lhs_type == "Ket"
+        op_name, label_args = leftside_ket(left)
+    else # type_str == "Bra"
+        op_name, label_args = leftside_bra(left)
+    end
+
+    return OpDefStr(op_name, label_args, lhs_type, prune_dirac(right))
+end
+
+immutable OpDefExpr
+    op_sym::Symbol
+    label_args::Union(Symbol, Expr)
+    lhs_type::Symbol
+    rhs::Union(Symbol, Expr)
+end
+
+OpDefExpr(ods::OpDefStr) = OpDefExpr(symbol(ods.op_name), parse(ods.label_args), symbol(ods.lhs_type), parse(ods.rhs))
 
 rm_whspace(str) = join(split(str, r"\s"))
 
@@ -63,119 +90,100 @@ function leftside_bra(str)
     end
 end
 
-function parse_defstr(str)
-    str = rm_whspace(str)
-    left, right = @compat(split(str, '=', limit=2))
-
-    if ismatch(ktpat, left)
-        type_str = "Ket"
-    elseif ismatch(brpat, left)
-        type_str = "Bra"
-    else
-        error("Couldn't detect whether operator is acting on Bra or Ket...")
-    end
-
-    if type_str == "Ket"
-        op_name, label_arg = leftside_ket(left)
-    else # type_str == "Bra"
-        op_name, label_arg = leftside_bra(left)
-    end
-
-    return (op_name, label_arg, prune_dirac(right), type_str)
-end
-
-function rep_op_expr(op_name, label_arg, def_str, type_str, basis)
-    op_sym = symbol(op_name)
-    label_expr = parse(label_arg)
-    def_expr = parse(def_str)
-    if type_str == "Ket"
-        return gen_ket_repr_expr(op_sym, label_expr, def_expr, basis)
-    else
-        return gen_bra_repr_expr(op_sym, label_expr, def_expr, basis)
-    end
-    return ex
-end
-
-function gen_ket_repr_expr(op_sym, label_expr, ket_expr, basis)
-    if isa(label_expr, Expr)
-        ex = quote 
-            local f($(label_expr.args...)) = $ket_expr * bra($(label_expr.args...))
-            $op_sym = sum(args->f(args...), $basis)
-        end
-    else
-        ex = quote 
-            $op_sym = sum($label_expr -> $ket_expr * bra($label_expr), $basis)
-        end
-    end
-    return ex
-end
-
-function gen_bra_repr_expr(op_sym, label_expr, bra_expr, basis)
-    if isa(label_expr, Expr)
-        ex = quote 
-            local f($(label_expr.args...)) = ket($(label_expr.args...)) * $bra_expr
-            $op_sym = sum(args->f(args...), $basis)
-        end
-    else
-        ex = quote 
-            $op_sym = sum($label_expr -> ket($label_expr) * $bra_expr, $basis)
-        end
-    end
-    return ex
-end
-
 ###########
 # @def_op #
 ###########
 
 macro def_op(str)
-    op_name, label_args, def_str, type_str = parse_defstr(str)
-    result_expr = def_op_expr(op_name, label_args, def_str, type_str)
+    result_expr = def_op_expr(OpDefStr(str))
     return esc(result_expr)
 end 
 
-function def_op_expr(op_name, label_args, def_str, type_str)
-    op_sym = symbol(op_name)
-    type_sym = symbol(type_str)
-    label_expr = parse(label_args)
-    def_expr = parse(def_str)
+function def_op_expr(ods::OpDefStr)
+    odex = OpDefExpr(ods)
 
-    func_label = symbol("_" * op_name * "_on_" * type_str * "_label")
-    func_pair = symbol("_" * op_name * "_on_" * type_str * "_pair")
-
-    if isa(label_expr, Expr)
-        func_label_def = quote 
-            $(func_label)($(label_expr.args...)) = $def_expr 
-            $(func_label)(label::StateLabel) = $(func_label)(label...)
+    if isa(odex.label_args, Expr)
+        func_on_label_def = quote 
+            local func_on_args($(odex.label_args.args...)) = $(odex.rhs)
+            local func_on_label(label::StateLabel) = func_on_args(label...)
         end
     else # isa(label_expr, Symbol)
-        func_label_def = quote 
-            $(func_label)($label_expr) = $def_expr 
-            $(func_label)(label::StateLabel) = $(func_label)(first(label))
+        func_on_label_def = quote 
+            local func_on_args($(odex.label_args)) = $(odex.rhs)
+            local func_on_label(label::StateLabel) = func_on_args(first(label))
         end
     end
 
-    if type_sym == :Ket
-        coeff_sym = :c
-    else
-        coeff_sym = :(c')
-    end
+    coeff = coeff_sym(odex)
 
     result = quote
-        $func_label_def
+        $func_on_label_def
 
-        function $(func_pair)(pair::Tuple)
+        local func_on_pair;
+        function func_on_pair(pair::Tuple)
           label, c = pair
-          return $(coeff_sym) * $(func_label)(label)
+          return $(coeff) * func_on_label(label)
         end  
 
-        $(op_sym)(state::$(type_sym)) = sum($(func_pair), QuDirac.dict(state))
+        $(odex.op_sym)(state::$(odex.lhs_type)) = sum(func_on_pair, QuDirac.dict(state))
     end
 
     return result
 end
 
+coeff_sym(odex::OpDefExpr) = odex.lhs_type == :Ket ? :c : :(c')
+
+###########
+# @rep_op #
+###########
+
+macro rep_op(str, bases...)
+    result_expr = rep_op_expr(OpDefStr(str), build_prod_basis(bases...))
+    return esc(result_expr)
+end 
+
+build_prod_basis(basis) = basis
+build_prod_basis(first, bases...) = :(Iterators.product($first, $(bases...)))
+
+function rep_op_expr(ods::OpDefStr, basis)
+    odex = OpDefExpr(ods)
+    if odex.lhs_type == :Ket
+        return gen_ket_repr_expr(odex, basis)
+    else
+        return gen_bra_repr_expr(odex, basis)
+    end
+    return ex
+end
+
+function gen_ket_repr_expr(odex::OpDefExpr, basis)
+    if isa(odex.label_args, Expr)
+        ex = quote 
+            local func_on_args($(odex.label_args.args...)) = $(odex.rhs) * bra($(odex.label_args.args...))
+            $(odex.op_sym) = sum(args->func_on_args(args...), $basis)
+        end
+    else
+        ex = quote 
+            $(odex.op_sym) = sum($(odex.label_args) -> $(odex.rhs) * bra($(odex.label_args)), $basis)
+        end
+    end
+    return ex
+end
+
+function gen_bra_repr_expr(odex::OpDefExpr, basis)
+    if isa(odex.label_args, Expr)
+        ex = quote 
+            local func_on_args($(odex.label_args.args...)) = ket($(odex.label_args.args...)) * $(odex.rhs)
+            $(odex.op_sym) = sum(args->func_on_args(args...), $basis)
+        end
+    else
+        ex = quote 
+            $(odex.op_sym) = sum($(odex.label_args) -> ket($(odex.label_args)) * $(odex.rhs), $basis)
+        end
+    end
+    return ex
+end
+
 export @d_str,
        @d_mstr,
-       @rep_op,
-       @def_op
+       @def_op,
+       @rep_op
