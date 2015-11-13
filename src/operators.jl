98a400d58b83dab34b2977687380759b16c6fc12
Jarrett Revels <jarrettrevels@gmail.com>
2015-11-12 23:55:27 -0500

    update FunctionLabel --> OpLabel, fields can be any callable, label space for bras and kets can be different

--- src/operators.jl
+++ src/operators.jl
@@ -10,34 +10,38 @@
 #   D = tensor(C, C)
 #   result = D * | 1, 2, 3 >
 
-#################
-# FunctionLabel #
-#################
-immutable FunctionLabel{P<:InnerProduct,L} <: AbstractLabel
-    onket::Function # onket(::StateLabel) -> AbstractKet{P,L}
-    onbra::Function # onbra(::StateLabel) -> AbstractKet{P,L}
+###########
+# OpLabel #
+###########
+immutable OpLabel{P<:InnerProduct,K,B} <: AbstractLabel
+    onket # onket(::StateLabel) -> AbstractKet{P,K}
+    onbra # onbra(::StateLabel) -> AbstractBra{P,B}
+    function OpLabel(onket, onbra)
+        @assert length(K.parameters) == length(B.parameters)
+        return new(onket, onbra)
+    end
 end
 
-@generated function tensor{P,A,B}(a::FunctionLabel{P,A}, b::FunctionLabel{P,B})
-    aparams, bparams = A.parameters, B.parameters
-    L = Tuple{aparams..., bparams...}
-    i = length(aparams)
+@generated function tensor{P,K1,B1,K2,B2}(a::OpLabel{P,K1,B1}, b::OpLabel{P,K2,B2})
+    K = Tuple{K1.parameters..., K2.parameters...}
+    B = Tuple{B1.parameters..., B2.parameters...}
+    i = length(K1.parameters)
     quote
-        function onket{$L}(lbl::StateLabel{$L})
+        function onket{$K}(lbl::StateLabel{$K})
             albl, blbl = split(lbl, Val{$i})
             akt = a.onket(albl)
             bkt = b.onket(blbl)
             return tensor(akt, bkt)
         end
 
-        function onbra{$L}(lbl::StateLabel{$L})
+        function onbra{$B}(lbl::StateLabel{$B})
             albl, blbl = split(lbl, Val{$i})
             abr = a.onbra(albl)
             bbr = b.onbra(blbl)
             return tensor(abr, bbr)
         end
 
-        return FunctionLabel{P,$L}(onket, onbra)
+        return OpLabel{P,$K,$B}(onket, onbra)
     end
 end
 
@@ -52,9 +56,9 @@ abstract AbstractOperator{P,L,T} <: AbstractDirac{P}
 # Operator Types #
 #----------------#
 immutable BasisOperator{P,L,T} <: AbstractOperator{P,L,T}
-    data::LabelTerm{FunctionLabel{L},T}
+    data::LabelTerm{OpLabel{L},T}
 end
 
-immutable OperatorSum{P,L,T} <: AbstractOperator{P,L,T}
-    data::LabelSum{FunctionLabel{L},T}
+type OperatorSum{P,L,T} <: AbstractOperator{P,L,T}
+    data::LabelSum{OpLabel{L},T}
 end
